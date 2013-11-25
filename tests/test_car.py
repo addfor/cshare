5665478a6ca7a7605a7681c58eb064d194295471
dani <dani84bs@gmail.com>
2013-11-25 14:11:57 +0100

    Added test for cshare.model.Car get_seats method.

--- tests/test_car.py
+++ tests/test_car.py
@@ -3,7 +3,12 @@ import unittest
 from cshare.model import Car
 
 class TestCar(unittest.TestCase):
-    def test__no_seats(self):
+    def test_no_seats(self):
         "A car must have at least one seat"
         self.assertRaises(Exception, Car, 0)
+
+    def test_car_creation(self):
+        for i in xrange(1,10):
+            car = Car(i)
+            self.assertEqual(i, car.get_seats())
 
