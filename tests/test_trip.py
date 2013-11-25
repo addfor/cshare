da533aee8b6eaf72829707d142c060c50472d6d2
dani <dani84bs@gmail.com>
2013-11-25 15:43:29 +0100

    Tests for cshare.model.Trip

---
+++ tests/test_trip.py
@@ -0,0 +1,18 @@
+import unittest
+
+SEATS = 3
+USER_NAME = "User_01"
+START_POINT = "Turin"
+END_POINT = "Turku"
+from cshare.model import (Car, Trip, Profile)
+
+class TestTrip(unittest.TestCase):
+    def setUp(self):
+        self.car = Car(SEATS)
+        self.user = Profile(USER_NAME, self.car)
+
+    def test_trip(self):
+        trip = Trip(START_POINT, END_POINT, self.user)
+        self.assertEqual(trip.get_start(), START_POINT)
+        self.assertEqual(trip.get_end(), END_POINT)
+        self.assertEqual(trip.get_user(), self.user)
